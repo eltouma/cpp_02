5930e55ed906a8869a83fc6e4e791569de1de859
User <user@MacBook-Pro-de-User.local>
2024-09-27 21:01:12 +0200

    Add comment to explain why what i wanted to do yesterday didn't work

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,6 +6,18 @@ Point::Point(void) : _x(0), _y(0)
 
 Point::Point(float const x, float const y) : _x(x), _y(y)
 {
+/*
+Ça ne fonctionne pas parce que :
+For members of fundamental types, it makes no difference which of the ways above the constructor is defined, because they are not initialized by default, but for member objects (those whose type is a class), if they are not initialized after the colon, they are default-constructed.
+Class Point  has a member object whose type is another class (base's type is Fixed). Because objects of class Fixed is constructed with a parameter, Point's constructor needs to call base's constructor, and the only way to do this is in the member initializer list.
+Source : https://cplusplus.com/doc/tutorial/classes/
+
+	this->_x = x;
+	this->_y = y;
+
+Si on ajoute une autre variable privée à Point qui n'est pas de type Fixed, comme int _i; par exemple, on aurait pu faire (en passant un 3ème param) :
+	this->_i = i
+*/
 }
 
 Point::Point(const Point& obj) : _x(obj.getPointX()), _y(obj.getPointY())
